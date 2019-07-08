f564d04bc4408a2a0592496dd0443d6d02e4934e
Alan <hernandezalan299@gmail.com>
2019-07-08 13:49:11 -0700

    initial commit with part of data of loader

---
+++ data.py
@@ -0,0 +1,33 @@
+import os
+import pandas as pd
+import numpy as np
+from torch.utils.data import Dataset, Dataloader
+from torchvision import transforms, utils
+
+class Dataset300W(Dataset):
+   def __init__(self, csv_file, root_dir, transform=None):
+      """
+      Args:
+         csv_file (string): path to the CSV file
+         root_dir (string): Directory to all of the images
+         transform (callable, optional): Optional transform to be applied
+                                         at retrieval time
+      """
+      self.root_dir = root_dor
+      self.transform = transform
+   def __getitem__(self, idx):
+      #to be filled in
+   def __len__(self):
+      #must return the length of this dataset
+
+class CKDataset(Dataset):
+   def __init__(self, csv_file, root_dir, transform=None):
+      #constructor 
+
+   def __getitem__(self, idx):
+      #override the index operator
+
+   def __len__(self):
+      #override the 
+
+class 
