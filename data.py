48104b17b153e6d9be4c62ddb99f8d66f6ff14ce
Oh <ahernandez@smtp.ict.usc.edu>
2019-07-09 12:32:20 -0700

    Update

--- data.py
+++ data.py
@@ -4,6 +4,9 @@ import numpy as np
 from torch.utils.data import Dataset, Dataloader
 from torchvision import transforms, utils
 
+#Dataset paths
+_300WCSV = "/data/deep/Alan/FacialEncodingDataset-OpenFace/300W-Processed/300W.csv"
+
 """300 faces in the wild dataset class"""
 class Dataset300W(Dataset):
    def __init__(self, csv_file, root_dir, transform=None):
@@ -15,15 +18,24 @@ class Dataset300W(Dataset):
                                          at retrieval time
       """
       self.root_dir = root_dor
+      self.df = pd.read_csv(csv_file)
       self.transform = transform
+
+   #Returns the ith row of our dataframe
    def __getitem__(self, idx):
-      #to be filled in
+      #assuming no transformation
+      return self.df.loc[idx]
+
    def __len__(self):
       #must return the length of this dataset
+      return len(self.df["frame"])
 
+"""Conh-Kanade+ dataset class"""
 class CKDataset(Dataset):
    def __init__(self, csv_file, root_dir, transform=None):
-      #constructor 
+      self.root_dir = root_dir
+      self.df = pd.read_csv(csv_file)
+      self.transform = transform
 
    def __getitem__(self, idx):
       #override the index operator
