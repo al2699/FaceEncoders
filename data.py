aec35c2c2b749fdecd0110e2efb3cef01ba61864
al2699 <ahernandez@smtp.ict.usc.edu>
2019-07-10 11:54:41 -0700

    Done with dataloaders

--- data.py
+++ data.py
@@ -8,7 +8,6 @@ from torchvision import transforms, utils
 W300_CSV = "/data/deep/Alan/FacialEncodingDataset-OpenFace/300W-Processed/300W.csv"
 CK_CSV = "/data/deep/Alan/FacialEncodingDataset-OpenFace/Ck+-Processed/CK+.csv"
 
-
 """300 faces in the wild dataset class"""
 class W300Dataset(Dataset):
    def __init__(self, csv_file, root_dir, transform=None):
@@ -34,15 +33,19 @@ class W300Dataset(Dataset):
       #assuming no transformation
       image_path = self.images["image_path"][idx]
       image = cv2.imread(image_path)
-      label = np.asarray(self.labels.loc[idx].tolist())
-      return image, label
+      if(self.transform == True):
+         image = self.transform(image)
+      #MAYBE CHANGE TO FLOAT16 LATER
+      label = np.asarray(self.labels.loc[idx].tolist(), dtype=np.float16)
+      #return a tuple of image 
+      return (image, label)
 
    def __len__(self):
       #must return the length of this dataset
       return len(self.images["image_path"])
 
-"""Cohn-Kanade Plus dataset class"""
-class W300Dataset(Dataset):
+"""Cohn-Kanade dataset class"""
+class CKDataset(Dataset):
    def __init__(self, csv_file, root_dir, transform=None):
       """
       Args:
@@ -60,47 +63,20 @@ class W300Dataset(Dataset):
       tempDF = pd.read_csv(csv_file)
       self.labels = tempDF.drop(["image_path"], axis=1)
       self.transform = transform
-
+,
    #Returns the ith row of our dataframe
    def __getitem__(self, idx):
       #assuming no transformation
       image_path = self.images["image_path"][idx]
       image = cv2.imread(image_path)
-      label = np.asarray(self.labels.loc[idx].tolist())
-      return image, label
+      if(self.transform == True):
+         image = self.transform(image)
+      #MAYBE CHANGE TO FLOAT16 LATER
+      label = np.asarray(self.labels.loc[idx].tolist(), dtype=np.float16)
+      #return a tuple of image 
+      return (image, label)
 
    def __len__(self):
       #must return the length of this dataset
       return len(self.images["image_path"])
 
-"""BP4D dataset class"""
-class W300Dataset(Dataset):
-   def __init__(self, csv_file, root_dir, transform=None):
-      """
-      Args:
-         csv_file (string): path to the CSV file
-         root_dir (string): Directory to all of the images
-         transform (callable, optional): Optional transform to be applied
-                                         at retrieval time
-      """
-      self.root_dir = root_dir
-      tempDF = pd.read_csv(csv_file)
-      for h in df.head():
-         if(h != "image_path"):
-            tempDF = tempDF.drop([h],axis=1)
-      self.images = tempDF
-      tempDF = pd.read_csv(csv_file)
-      self.labels = tempDF.drop(["image_path"], axis=1)
-      self.transform = transform
-
-   #Returns the ith row of our dataframe
-   def __getitem__(self, idx):
-      #assuming no transformation
-      image_path = self.images["image_path"][idx]
-      image = cv2.imread(image_path)
-      label = np.asarray(self.labels.loc[idx].tolist())
-      return image, label
-
-   def __len__(self):
-      #must return the length of this dataset
-      return len(self.images["image_path"])
