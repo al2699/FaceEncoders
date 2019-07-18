591b89df93a2afdf419f8a7860aa334db0b08a97
Alan Hernandez <ahernandez@smtp.ict.usc.edu>
2019-07-18 16:06:37 -0700

    Added BP4D dataset class in data.py and modified the create_split script to create the BP4D datasplits

--- data.py
+++ data.py
@@ -63,8 +63,8 @@ class W300Dataset(Dataset):
       #must return the length of this dataset
       return len(self.images["image_path"])
    
-   Returns a list of the indices which include the rows of data for each
-   respective portion of the split
+   #Returns a list of the indices which include the rows of data for each
+   #respective portion of the split
    def train_test_validation_split(self):
       tempDF = pd.read_csv(W300_split_list)
       train_list = tempDF["train"].tolist()
@@ -73,9 +73,62 @@ class W300Dataset(Dataset):
 
       return train_list, test_list, valid_list
 
+
 """Cohn-Kanade dataset class"""
 class CKDataset(Dataset):
    def __init__(self, csv_file=CK_CSV, transform=None):
+      """
+      Args:
+         csv_file (string): path to the CSV file
+         root_dir (string): Directory to all of the images
+         transform (callable, optional): Optional transform to be applied
+                                         at retrieval time
+      """
+      #self.root_dir = root_dir
+      print(csv_file)
+      tempDF = pd.read_csv(csv_file)
+      for h in tempDF.head():
+         if(h != "image_path"):
+            tempDF = tempDF.drop([h],axis=1)
+      self.images = tempDF
+      tempDF = pd.read_csv(csv_file)
+      self.labels = tempDF.drop(["image_path"], axis=1)
+      self.transform = transform
+
+   #Returns the ith row of our dataframe
+   def __getitem__(self, idx):
+      #assuming no transformation
+      image_path = self.images["image_path"][idx]
+      image = cv2.imread(image_path)
+      #print("Extracted: " + image_path)
+      if(image.all() == None):
+         print("Missing: " + image_path)
+      if(self.transform == True):
+         image = self.transform(image)
+      image_tensor = torch.Tensor(image)
+      image = Variable(image_tensor, requires_grad=False)
+      #MAYBE CHANGE TO FLOAT16 LATER
+      label = np.asarray(self.labels.loc[idx].tolist(), dtype=np.float16)
+      label_tensor = torch.Tensor(label).view(-1,1)
+      label = Variable(label_tensor, requires_grad=False)
+      #return a tuple of image 
+      return (image, label)
+
+   def __len__(self):
+      #must return the length of this dataset
+      return len(self.images["image_path"])
+
+   def train_test_validation_split(self):
+      tempDF = pd.read_csv(W300_split_list)
+      train_list = tempDF["train"].tolist()
+      test_list = tempDF["test"].tolist()
+      valid_list = tempDF["validation"].tolist()
+
+      return train_list, test_list, valid_list
+
+"""BP4D  dataset class"""
+class BP4DDataset(Dataset):
+   def __init__(self, csv_file=BP4D_CSV, transform=None):
       """
       Args:
          csv_file (string): path to the CSV file
