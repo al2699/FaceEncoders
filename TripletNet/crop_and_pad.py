ba3173c09af00bc9801d7c929b1d2bb4948f5294
Alan <hernandezalan299@gmail.com>
2019-07-27 21:12:08 -0700

    Updated script

--- TripletNet/crop_and_pad.py
+++ TripletNet/crop_and_pad.py
@@ -2,7 +2,21 @@ import torchvision.transforms as t
 from PIL import Image
 import pandas as pd
 
+#Helper function
+def get_pil_image(img_path):
+   toTensor = t.ToTensor()
+   toPImage = t.ToPILImage()
+   return toPImage(toTensor(img_path))
+
+def pad_iamge(img_path, side):
+   p_img = get_pil_image(img_path
+)
+   if side == "height":
+      
+   else:
+
 csv_path = ""
 
 df = pd.read_csv(csv_path)
+
 
