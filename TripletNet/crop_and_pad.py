a2c0c72252fd0d86bf24056fd4d0e65e0df091d3
Alan Hernandez <ahernandez@smpt.ict.usc.edu>
2019-07-29 12:35:03 -0700

    Need to test crop and pad.py

--- TripletNet/crop_and_pad.py
+++ TripletNet/crop_and_pad.py
@@ -34,23 +34,27 @@ def get_crop_image(index, triplet_num):
    crop_img = p_img.crop(box)
    return crop_img
 
-def pad_iamge(p_img):
-   aspect_ratio = p_img.size[0] / p_img.size[1]
-   one_over_ar = p_img.size[0] / p_img.size[1]
-   if(aspect_ratio < 1):
-      #Need to pad width
-      
-   elif(one_over_ar < 1):
-      #Need to pad height
-   else:
-      #already square image
-      return p_img
+def pad_image(p_img):
+   desired_size = 224
+   old_size = p_img.size  # old_size[0] is in (width, height) format
 
-   if side == "height":
-      #Then we pad the the height until height == width
-            
-   else:
-      #Then we pad the width until height == width
-df = pd.read_csv(csv_path)
+   ratio = float(desired_size)/max(old_size)
+   new_size = tuple([int(x*ratio) for x in old_size])
+   # use thumbnail() or resize() method to resize the input image
 
+   # thumbnail is a in-place operation
 
+   # im.thumbnail(new_size, Image.ANTIALIAS)
+
+   im = p_img.resize(new_size, Image.ANTIALIAS)
+   # create a new image and paste the resized on it
+
+   new_im = Image.new("RGB", (desired_size, desired_size))
+   new_im.paste(im, ((desired_size-new_size[0])//2,
+                    (desired_size-new_size[1])//2))
+
+   new_im.show()
+
+if __name__ == "__main__":
+   p_img = get_crop_image(0, 1)
+   pad_image(p_img)
